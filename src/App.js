4b07a04ef0d5f7b982209d5cf1038335c1ca99b2
Bharathula Prasad <KrishnaPrasad.Bharathula@gmail.com>
2021-12-28 11:43:16 +0100

    my-reac-app sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// React Native Counter Example using Hooks!
 
-function App() {
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View style={styles.container}>
+      <Text>You clicked {count} times</Text>
+      <Button onPress={() => setCount(count + 1)} title="Click me!" />
+    </View>
   );
-}
+};
+
+// React Native Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default App;
