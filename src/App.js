dc43aa3d78c73e7e38b97b375df070fde7cfbc7b
Bharathula Prasad <KrishnaPrasad.Bharathula@gmail.com>
2021-12-28 12:22:18 +0100

    demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // React Native Counter Example using Hooks!
 
 import React, { useState } from "react";
+
 import { View, Text, Button, StyleSheet } from "react-native";
 
 const App = () => {
@@ -10,14 +11,18 @@ const App = () => {
     <View style={styles.container}>
       <Text>You clicked {count} times</Text>
       <Button onPress={() => setCount(count + 1)} title="Click me!" />
-      <Cat name="Krishna" />
-      <Cat name="Bharathula" />
+      <Cat name="Krishna Bharathula" />
+      <Copy name="Copyright 2022" />
     </View>
   );
 };
 
 const Cat = (props) => {
   return <Text>Hello, I am {props.name}!</Text>;
+};
+
+const Copy = (props) => {
+  return <Text>{props.name}!</Text>;
 };
 
 // React Native Styles
